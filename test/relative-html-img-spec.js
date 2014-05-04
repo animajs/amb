ff98b01563145cde5e15d78a7b168493fc356319
chencheng <sorrycc@gmail.com>
2014-05-04 17:08:53 +0800

    rename

--- test/relative-html-img-spec.js
+++ test/relative-html-img-spec.js
@@ -4,10 +4,10 @@ var path = require('path');
 var fs = require('fs');
 var gulp = require('gulp');
 var es = require('event-stream');
-var uploadImg = require('../lib/html').uploadImg;
+var relativeHTMLImg = require('../lib/html').relativeHTMLImg;
 var util = require('../lib/util');
 
-describe('uploadImg', function() {
+describe('relativeHTMLImg', function() {
 
   // Fake util.cdn
   var _cdn = util.cdn;
@@ -23,21 +23,21 @@ describe('uploadImg', function() {
   });
 
   it('do not upload if no img', function() {
-    uploadImg('<div></div>').should.be.eql('<div></div>');
+    relativeHTMLImg('<div></div>').should.be.eql('<div></div>');
   });
 
   it('upload with images', function(done) {
-    uploadImg(fs.readFileSync(path.join(__dirname, './fixtures/upload-img/a.html'), 'utf-8'), path.join(__dirname, './fixtures/upload-img/'), function(e, html) {
-      html.should.be.eql(fs.readFileSync(path.join(__dirname, './fixtures/upload-img/a-expected.html'), 'utf-8'));
+    relativeHTMLImg(fs.readFileSync(path.join(__dirname, './fixtures/relative-html-img/a.html'), 'utf-8'), path.join(__dirname, './fixtures/relative-html-img/'), function(e, html) {
+      html.should.be.eql(fs.readFileSync(path.join(__dirname, './fixtures/relative-html-img/a-expected.html'), 'utf-8'));
       done();
     });
   });
 
   it('upload images with gulp', function(done) {
-    gulp.src('test/fixtures/upload-img/a.html')
-      .pipe(uploadImg.gulp())
+    gulp.src('test/fixtures/relative-html-img/a.html')
+      .pipe(relativeHTMLImg.gulp())
       .pipe(es.map(function(file, cb) {
-        String(file.contents).should.be.eql(fs.readFileSync(path.join(__dirname, './fixtures/upload-img/a-expected.html'), 'utf-8'));
+        String(file.contents).should.be.eql(fs.readFileSync(path.join(__dirname, './fixtures/relative-html-img/a-expected.html'), 'utf-8'));
         cb(null, file);
       }))
       .on('end', done);
